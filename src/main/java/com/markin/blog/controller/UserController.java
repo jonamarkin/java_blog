bce4904eb360e228b99b1681baa48028ab914b4a
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-04 21:51:30 +0200

    Users controller skeleton

--- src/main/java/com/markin/blog/controller/UserController.java
+++ src/main/java/com/markin/blog/controller/UserController.java
@@ -1,3 +1,29 @@
 package com.markin.blog.controller;
 
-public class UserController {}
+import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.PutMapping;import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/users")
+public class UserController {
+    //User signup
+    @PostMapping("/signup")
+    public void signup() {
+        //TODO
+    }
+    //User login
+    @PostMapping("/login")
+    public void login() {
+        //TODO
+    }
+    //User logout
+    @PostMapping("/logout")
+    public void logout() {
+        //TODO
+    }
+    //User update
+    @PutMapping("/update")
+    public void update() {
+        //TODO
+    }
+
+
+}
