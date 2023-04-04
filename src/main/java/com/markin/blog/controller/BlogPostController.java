d91d15c5b30e1cffbffdfeb6e82d9c67710bc1f5
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-04 21:59:48 +0200

    Blog Post and category controllers

--- src/main/java/com/markin/blog/controller/BlogPostController.java
+++ src/main/java/com/markin/blog/controller/BlogPostController.java
@@ -1,3 +1,37 @@
 package com.markin.blog.controller;
 
-public class BlogPostController {}
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/api/v1/posts")
+public class BlogPostController {
+    //Create a new post
+    @PostMapping("/create")
+    public void create() {
+        //TODO
+    }
+    //Update a post
+    @PutMapping("/update")
+    public void update() {
+        //TODO
+    }
+    //Delete a post
+    @DeleteMapping("/delete")
+    public void delete() {
+        //TODO
+    }
+    //Get a post
+    @GetMapping("/view")
+    public void get() {
+        //TODO
+    }
+    //Get all posts
+    @GetMapping("/view/all")
+    public void getAll() {
+        //TODO
+    }
+    //Get all posts by user
+    @GetMapping("/view/all/user")
+    public void getAllByUser() {
+        //TODO
+    }
+}
--- src/main/java/com/markin/blog/controller/CategoryController.java
+++ src/main/java/com/markin/blog/controller/CategoryController.java
@@ -1,3 +1,32 @@
 package com.markin.blog.controller;
 
-public class CategoryController {}
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/api/v1/categories")
+public class CategoryController {
+  // Create a new category
+  @PostMapping("/create")
+  public void create() {
+    // TODO
+  }
+  // Update a category
+  @PutMapping("/update")
+  public void update() {
+    // TODO
+  }
+  // Delete a category
+  @DeleteMapping("/delete")
+  public void delete() {
+    // TODO
+  }
+    //Get a category
+    @GetMapping("/view")
+    public void get() {
+        //TODO
+    }
+  // Get all categories
+  @GetMapping("/view/all")
+  public void getAll() {
+    // TODO
+  }
+}
