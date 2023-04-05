b691bcdcc454820efdab90b4544080e640769b29
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-05 16:49:44 +0200

    User and post services

---
+++ src/main/java/com/markin/blog/service/BlogPostService.java
@@ -0,0 +1,38 @@
+package com.markin.blog.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogPostService {
+    //TODO: Implement BlogPostService
+
+    //Create a new post
+    public void create() {
+
+    }
+
+    //Update a post
+    public void update() {
+
+    }
+
+    //Delete a post
+    public void delete() {
+
+    }
+
+    //Get a post
+    public void get() {
+
+    }
+
+    //Get all posts
+    public void getAll() {
+
+    }
+
+    //Get all posts by user
+    public void getAllByUser() {
+
+    }
+}
---
+++ src/main/java/com/markin/blog/service/UserService.java
@@ -0,0 +1,40 @@
+package com.markin.blog.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    //TODO: Implement UserService
+
+    //Register a new user
+    public void register() {
+
+    }
+
+    //Login a user
+    public void login() {
+
+    }
+
+    //Logout a user
+    public void logout() {
+
+    }
+
+    //Update a user
+    public void update() {
+
+    }
+
+    //Get a user
+    public void get() {
+
+    }
+
+    //Get all users
+    public void getAll() {
+
+    }
+
+
+}
