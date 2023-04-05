763d406020c20dc3f2b35ae37ca8dd5064be08d6
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-05 16:23:57 +0200

    Added more options to user repository

--- src/main/java/com/markin/blog/repository/BlogPostRepository.java
+++ src/main/java/com/markin/blog/repository/BlogPostRepository.java
@@ -1,3 +1,12 @@
 package com.markin.blog.repository;
 
-public interface BlogPostRepository {}
+import com.markin.blog.model.BlogPost;
+import com.markin.blog.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface BlogPostRepository extends MongoRepository<BlogPost, String> {
+    List<BlogPost> findByAuthor(User author);
+}
--- src/main/java/com/markin/blog/repository/UserRepository.java
+++ src/main/java/com/markin/blog/repository/UserRepository.java
@@ -8,4 +8,12 @@ import org.springframework.stereotype.Repository;import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
 
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
 }
